04d70fe385868ae45011188a776d3e99ad96db34
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2020-08-06 16:24:39 -0300

    New project

---
+++ airbnb-home/airbnb-home/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  airbnb-home
+//
+//  Created by Vinicius Carvalho Marques on 06/08/20.
+//  Copyright © 2020 Vinicius Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
