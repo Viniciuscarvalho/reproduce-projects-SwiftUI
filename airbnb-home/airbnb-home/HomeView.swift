95bd5253676e41bfc96a5c27bea1989c094167da
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2020-08-18 09:44:58 -0300

    Add carrousel and some for for 3 cards

--- airbnb-home/airbnb-home/HomeView.swift
+++ airbnb-home/airbnb-home/HomeView.swift
@@ -16,21 +16,43 @@ struct HomeView: View {
     var body: some View {
         
         // First view with big image
-        
-        ScrollView {
-            VStack {
-                home.image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: width, height: height)
+        GeometryReader { bounds in
+            ScrollView {
+                VStack {
+                    self.home.image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: self.width, height: self.height)
+                }
+                .cornerRadius(30)
+                .shadow(color: self.home.color.opacity(0.3), radius: 20, x: 0, y: 20)
             }
-            .cornerRadius(30)
-            .shadow(color: home.color.opacity(0.3), radius: 20, x: 0, y: 20)
+            
+            // Stack horizontal with images
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(homeData) { item in
+                        GeometryReader { geometry in
+                            HomeView(home: item)
+                                .rotation3DEffect(Angle(degrees:
+                                    Double(geometry.frame(in: .global).minX - 30) / -getAngleMultiplier(bounds: bounds)
+                                ), axis: (x: 0, y: 10, z: 0))
+                        }
+                        .frame(width: 275, height: 275)
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
         }
-        
-        // Stack horizontal with images
-        
-        
+    }
+}
+
+func getAngleMultiplier(bounds: GeometryProxy) -> Double {
+    if bounds.size.width > 500 {
+        return 80
+    } else {
+        return 20
     }
 }
 
@@ -42,11 +64,13 @@ struct Home: Identifiable {
 }
 
 let homeData = [
-    Home(text: "Get out and stretch your imagination", image: Image(uiImage: #imageLiteral(resourceName: "airbnb-home-cardPrincipal")), color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)))
+    Home(text: "Online Experiences", image: Image(uiImage: #imageLiteral(resourceName: "airbnb-home-card1")), color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))),
+    Home(text: "Unique stays", image: Image(uiImage: #imageLiteral(resourceName: "airbnb-home-card2")), color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))),
+    Home(text: "Entire homes", image: Image(uiImage: #imageLiteral(resourceName: "airbnb-home-card3")), color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)))
 ]
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(home: home)
+        HomeView(home: self)
     }
 }
